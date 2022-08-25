df244ef2c3195a5cfc03d03bd64619e2df6e66f5
ammar <ammar.hoque@shield.ai>
2022-08-25 10:34:20 -0700

    shebangs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 import gym
 import torch
 import numpy as np
---
+++ model.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+import torch
+import numpy as np
+class Actor:
+    def __init__(self, input_size):
+        pass    
+    
+class Critic:
+    def __init__(self, input_size):
+        pass
