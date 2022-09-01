6194458984470e3fba7572460b0e742092e92bd8
Ammar <ammar.hoque@shield.ai>
2022-09-01 14:26:15 -0700

    playing with env

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import gym
 import torch
 import numpy as np
 
-env = gym.make('BipedalWalker-v2')
+env = gym.make('BipedalWalker-v3')
 print("here's observation space: ", env.observation_space)
 print("here's action space: ", env.action_space)
-
+env.reset()
+while True:
+    env.render()
