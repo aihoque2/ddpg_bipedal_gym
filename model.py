a188ed57c9dab45a829d3ef924a5a64c55917c9d
aihoque2 <aihoque2@illinois.edu>
2022-10-14 20:54:48 -0700

    add dependenceies

--- model.py
+++ model.py
@@ -1,7 +1,14 @@
 #!/usr/local/bin/python3
 
+import gym, os
+from itertools import count
 import torch
-import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+
 class Actor:
     def __init__(self, input_size):
         pass    
